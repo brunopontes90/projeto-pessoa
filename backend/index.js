9ce8d4a60582de64eba0433209b9ce778fc06a15
Bruno Pontes <bruno_confessor@hotmail.com>
2023-01-16 13:53:15 -0300

    Criado api para get, post, put, delete

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const app = express();
 const mysql = require('mysql');
 const env = require('dotenv').config();
 
+const cors = require('cors');
+
 const db = mysql.createConnection({
     host: `${process.env.MYSQL_DOCKER_HOST}`,
     user: `${process.env.MYSQL_DOCKER_USER}`,
@@ -10,6 +12,9 @@ const db = mysql.createConnection({
     database: `${process.env.MYSQL_DOCKER_DATABASE}`,
     port: `${process.env.MYSQL_DOCKER_PORT}`
 });
+
+app.use(cors());
+app.use(express.json());
 
 db.connect(function (err) {
     if (err) {
@@ -19,16 +24,83 @@ db.connect(function (err) {
     }
 })
 
-app.get('/', (req, res) => {
-    res.send("Ola")
-    //let SQl = "SELECT * FROM pessoa";
-    //let SQl = "INSERT INTO pessoa ('nome', 'idade', 'cpf','endereco','numero_casa','complemento','sexo') VALUES ('Bruno Pontes', 32, 38866495808, 'Rua Thomaz de Araujo', 316, 'Casa 1', 'M') ";
+app.get('/getuser', (req, res) => {
 
-    /*db.query(SQl, (err, result) => {
-        console.log(err);
-    });*/
+    let SQl = "SELECT * FROM pessoa";
+    db.query(SQl, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
+app.post('/register', (req, res) => {
+
+    const { nome } = req.body;
+    const { idade } = req.body;
+    const { cpf } = req.body;
+    const { endereco } = req.body;
+    const { numero_casa } = req.body;
+    const { complemento } = req.body;
+    const { sexo } = req.body;
+    const { isAdmin } = req.body;
+
+    let SQl = "INSERT INTO pessoa (nome, idade, cpf, endereco, numero_casa, complemento, sexo, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ";
+
+    db.query(SQl, [nome, idade, cpf, endereco, numero_casa, complemento, sexo, isAdmin], (err, result) => {
+        console.log(err);
+    });
+});
+
+app.put('/edit', (req, res) => {
+
+    const { id } = req.body;
+    const { nome } = req.body;
+    const { idade } = req.body;
+    const { cpf } = req.body;
+    const { endereco } = req.body;
+    const { numero_casa } = req.body;
+    const { complemento } = req.body;
+    const { sexo } = req.body;
+    const { isAdmin } = req.body;
+
+    let SQl = "UPDATE pessoa SET nome = ?, idade = ?, cpf = ?, endereco = ?, numero_casa = ?, complemento = ?, sexo = ?, isAdmin = ? WHERE id = ?";
+
+    db.query(SQl,
+        [
+            nome,
+            idade,
+            cpf,
+            endereco,
+            numero_casa,
+            complemento,
+            sexo,
+            isAdmin,
+            id
+        ], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        });
+});
+
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    let SQl = "DELETE FROM pessoa WHERE id = ?";
+
+    db.query(SQl, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
 app.listen(3001, () => {
     console.log('Rodando server');
 })
